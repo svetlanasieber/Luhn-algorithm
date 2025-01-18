59b290dc9d88d72f033cd7646422c9c11cd2246a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-18 23:03:45 +0100

    Update CreditCardValidation.java

--- CreditCardValidation.java
+++ CreditCardValidation.java
@@ -1,43 +1,47 @@
+package AlgoRecap;
+
 import java.util.Scanner;
 
 public class CreditCardValidation {
-      public static void main(String[] args) {
-     Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         int count = 0;
-        long array[] = new long [16];
-       do
-       {
-        count = 0;
-       array = new long [16];
-        System.out.print("Enter your Credit Card Number : ");
-        long number = in.nextLong();
-        for (int i = 0; number != 0; i++) {
-        array[i] = number % 10;
-        number = number / 10;
-        count++;
+        long array[] = new long[16];
+        do {
+            count = 0;
+            array = new long[16];
+            System.out.print("Enter your Credit Card Number : ");
+            long number = in.nextLong();
+            for (int i = 0; number != 0; i++) {
+                array[i] = number % 10;
+                number = number / 10;
+                count++;
+            }
         }
-       }
-        while(count < 13); 
-        if ((array[count - 1] == 4) || (array[count - 1] == 5) || (array[count - 1] == 3 && array[count - 2] == 7)){
+        while (count < 13);
+        if ((array[count - 1] == 4) || (array[count - 1] == 5) || (array[count - 1] == 3 && array[count - 2] == 7)) {
             if (isValid(array) == true) {
                 System.out.println("\n The Credit Card Number is Valid. ");
+            } else {
+                System.out.println("\n The Credit Card Number is Invalid. ");
+            }
         } else {
             System.out.println("\n The Credit Card Number is Invalid. ");
-        }
-        } else{
-          System.out.println("\n The Credit Card Number is Invalid. ");
         }
     }
 
     public static boolean isValid(long[] array) {
-        int total = sumOfDoubleEvenPlace(array) + sumOfOddPlace(array);        
+        int total = sumOfDoubleEvenPlace(array) + sumOfOddPlace(array);
         if ((total % 10 == 0)) {
-         for (int i=0; i< array.length; i++){
-            System.out.println(array[i]);}
+            for (int i = 0; i < array.length; i++) {
+                System.out.println(array[i]);
+            }
             return true;
         } else {
-          for (int i=0; i< array.length; i++){
-            System.out.println(array[i]);}
+            for (int i = 0; i < array.length; i++) {
+                System.out.println(array[i]);
+            }
             return false;
         }
     }
@@ -54,27 +58,28 @@ public class CreditCardValidation {
 
     public static int sumOfOddPlace(long[] array) {
         int result = 0;
-        for (int i=0; i< array.length; i++)
-        {
-        while (array[i] > 0) {
-            result += (int) (array[i] % 10);
-            array[i] = array[i] / 100;
-         }}
-         System.out.println("\n The sum of odd place is " + result);
+        for (int i = 0; i < array.length; i++) {
+            while (array[i] > 0) {
+                result += (int) (array[i] % 10);
+                array[i] = array[i] / 100;
+            }
+        }
+        System.out.println("\n The sum of odd place is " + result);
         return result;
     }
 
     public static int sumOfDoubleEvenPlace(long[] array) {
         int result = 0;
         long temp = 0;
-        for (int i=0; i< array.length; i++){
-        while (array[i] > 0) {
-             temp = array[i] % 100;
-             result += getDigit((int) (temp / 10) * 2);
-            array[i] = array[i] / 100;
-           }
+        for (int i = 0; i < array.length; i++) {
+            while (array[i] > 0) {
+                temp = array[i] % 100;
+                result += getDigit((int) (temp / 10) * 2);
+                array[i] = array[i] / 100;
+            }
         }
         System.out.println("\n The sum of double even place is " + result);
         return result;
     }
-     }
+}
+    
